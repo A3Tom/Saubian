b90bc9ddf337098f8c0d673fe203edcca1f6f79d
A3Tom <Tom_Esq@Yahoo.com>
2021-04-17 21:46:44 +0100

    Added message bus handler Upgraded Honking ability

---
+++ src/Saubian.Service/Classes/MessageBusHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+
+namespace Saubian.Service.Classes
+{
+    public static class MessageBusHandler
+    {
+        public static async Task PeepThemGlyphs(MessageSender messagesQueue, Stream messageStream)
+        {
+            string requestBody = await new StreamReader(messageStream).ReadToEndAsync();
+
+            byte[] bytes = Encoding.ASCII.GetBytes(requestBody);
+            Message m1 = new Message(bytes);
+            await messagesQueue.SendAsync(m1);
+        }
+    }
+}
